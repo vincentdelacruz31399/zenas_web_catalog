05500743384d0f190aca29fbe915234c3808bddc
vincentdelacruz31399 <144637314+vincentdelacruz31399@users.noreply.github.com>
2023-10-05 10:39:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,21 @@
 import streamlit
 import snowflake.connector
-streamlit.title("My Parents Healthy Bar")
+import pandas
+
+streamlit.title("'Zena\'s Amazing Athleisure Catalog'")
 
 #making sure requirements, secrets, and connections are all working together 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+#streamlit.text("Hello from Snowflake:")
+#streamlit.text(my_data_row)
+
+#FINAL Code we used for Zena’s Web Catalog Prototype
+
+#put the dafta into a dataframe
+df=pandas.dataframe(my_catalog)
+
+# temp write the dataframe to the page so I Can see what I am working with
+streamlit.write(df)
